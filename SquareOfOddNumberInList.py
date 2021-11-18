a6eecda1dece1bb2c022c72bf3e2fdf9310ee3f5
Suraj Saurabh <surajsaurabh@ymail.com>
2021-11-18 20:08:38 +0530

    Square of odd number from a list

---
+++ SquareOfOddNumberInList.py
@@ -0,0 +1,6 @@
+# WAP to print square of odd number in the list
+a = [12, 48, 5, 25, 3, 32, 27, 1, 45, 22, 14, 81, 2, 51, 11, 4, 44, 52, 7, 44, 55, 22]
+
+for i in a:
+    if i % 2 == 1:
+        print(i * i)
