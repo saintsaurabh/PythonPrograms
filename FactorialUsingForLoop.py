9d77481bfba16277e85691c659c705ec7c9c7677
Suraj Saurabh <surajsaurabh@ymail.com>
2021-11-29 16:40:54 +0530

    Update

--- FactorialUsingForLoop.py
+++ FactorialUsingForLoop.py
@@ -1,9 +1,6 @@
 num = int(input("Enter the number: "))
 
 fact = 1
-if num >= 0:
-    for i in range(1, num+1):
-        fact = fact * i
-    print("Factorial of",num,"is", fact)
-else:
-    print("Invalid input.")
+for i in range(1, num+1):
+    fact = fact * i
+print("Factorial of",num,"is", fact)
