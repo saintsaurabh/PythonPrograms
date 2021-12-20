9f3628d082927ccbe67d537066d095cc8820741a
Suraj Saurabh <surajsaurabh@ymail.com>
2021-12-20 19:41:33 +0530

    Function(Calculator)

---
+++ CalculatorUsingFunction.py
@@ -0,0 +1,56 @@
+def add(n1, n2):
+
+    return n1+n2
+
+
+def sub(n1, n2):
+
+    return n1-n2
+
+
+def multi(n1, n2):
+
+    return n1*n2
+
+
+def div(n1, n2):
+
+    return n1//n2
+
+
+def get_data():
+    start = int(input("Enter the first number: "))
+    end = int(input("Enter the second number: "))
+    return start, end
+
+
+while True:
+
+    print("Press 1 for addition.")
+    print("Press 2 for subtraction.")
+    print("Print 3 for multiplication.")
+    print("Print 4 for division.")
+    print("Print 5 for stop.")
+    choice = int(input("Enter any of the choice given above: "))
+
+    if choice == 1:
+        data = get_data()
+        print(add(*data))
+
+    elif choice == 2:
+        data = get_data()
+        print(sub(*data))
+
+    elif choice == 3:
+        data = get_data()
+        print(multi(*data))
+
+    elif choice == 4:
+        data = get_data()
+        print(div(*data))
+
+    elif choice == 5:
+        break
+
+    else:
+        print("Valid input choice.")
